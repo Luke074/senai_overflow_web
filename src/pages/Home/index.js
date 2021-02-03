b6c07e6d9c46b9b697f93be35285494b9145c873
Luke074 <mendeslucas277@gmail.com>
2021-02-03 16:59:07 -0300

    implementando os comentarios(respostas)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,41 +5,9 @@ import logo from "../../assets/logo.png";
 import { FaSignOutAlt } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-import { singOut } from "../../services/security";
+import { singOut, getUser } from "../../services/security";
 import { useHistory } from "react-router-dom";
 
-function Question({ question }) {
-    return (
-        <QuestionCard>
-            <header>
-                <img src={fotoPerfil} alt="fotoPerfil" />
-                <strong>pro {question.Student.name}</strong>
-                <p>em {question.created_at}</p>
-            </header>
-            <section>
-                <strong>{question.title}</strong>
-                <p>{question.description}</p>
-                <img src={question.image} />
-            </section>
-            <footer>
-                <h1> 11 respostas</h1>
-                <section>
-                    <header>
-                        <img src={fotoPerfil} alt="fotoPerfil" />
-                        <strong>Por fulano</strong>
-                        <p> 12/12/2012 as 12:12</p>
-                    </header>
-                    <p>Respostas para a pergunta</p>
-                </section>
-                <form>
-                    <textarea placeholder="Responda essa duvida" rows="2" required></textarea>
-                    <button>Enviar</button>
-                </form>
-
-            </footer>
-        </QuestionCard>
-    );
-}
 
 function Profile() {
     return (
@@ -61,6 +29,109 @@ function Profile() {
                 <p>lukeabobrinha@senai.com</p>
             </section>
         </>
+    );
+}
+
+function Answers({ answers }) {
+    return (
+        <section>
+            <header>
+                <img src={fotoPerfil} alt="fotoPerfil" />
+                <strong>Por {answers.Student.name}</strong>
+                <p> em {answers.created_at}</p>
+            </header>
+            <p>{answers.description}</p>
+        </section>
+    );
+}
+
+function Question({ question }) {
+
+    const [showAnswers, setShowAnswers] = useState();
+
+    const [newAnswers, setNewAnswers] = useState(question.Answers);
+
+    const [answers, setAnswers] = useState({
+        description: "",
+    });
+
+    const handleSubmit = async (a) => {
+        a.preventDefault();
+
+        if(answers.length > 10)
+            return alert("A resposta deve ter no minimo 10 caracteres");
+
+        try {
+            const response = await api.post(`/questions/${question.id}/answers`, answers);
+
+            const aluno = getUser();
+
+            const answersAdded = {
+                id: response.data.id,
+                description: answers,
+                created_at: response.data.createdAt,
+                Student: {
+                    id: aluno.studentId,
+                    name: aluno.name,
+                },
+            }
+
+            setNewAnswers([...newAnswers, answersAdded]);
+
+        } catch (error) {
+            console.error(error);
+            alert(error.response.data.error);
+        }
+    }
+    const handleInsert = (a) => {
+        setAnswers({ ...answers, description: a.target.value });
+    }
+
+    const qntdAnswers = newAnswers.length;
+    return (
+        <QuestionCard>
+            <header>
+                <img src={fotoPerfil} alt="fotoPerfil" />
+                <strong>pro {question.Student.name}</strong>
+                <p>em {question.created_at}</p>
+            </header>
+            <section>
+                <strong>{question.title}</strong>
+                <p>{question.description}</p>
+                <img src={question.image} />
+            </section>
+            <footer>
+                <h1 onClick={() => setShowAnswers(!showAnswers)}>
+                    {qntdAnswers === 0 ? (
+                        "Seja o primeiro a Responder"
+                    ) : (
+                            <>
+                                {qntdAnswers}
+                                {qntdAnswers > 1 ? " Respostas" : " Resposta"}
+                            </>
+
+                        )}
+                </h1>
+                {showAnswers && (
+                    <>
+                        {newAnswers.map((a) => (<Answers answers={a} />))}
+                    </>
+                )}
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                        placeholder="Responda essa duvida"
+                        minLength="10"
+                        rows="2"
+                        value={answers.description}
+                        onChange={handleInsert}
+                        required
+                    >
+                    </textarea>
+                    <button>Enviar</button>
+                </form>
+
+            </footer>
+        </QuestionCard>
     );
 }
 
